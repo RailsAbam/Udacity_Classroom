71ba7b3086b48f45245c15024df4c5457ad21f81
Sunday AbamJnr <abamsunday5@gmail.com>
2021-09-04 23:48:51 +0100

    first commit

---
+++ Game.py
@@ -0,0 +1,13 @@
+
+
+#Guess = 20
+Guess = 20
+Guess = int(input("Guess a Number: "))
+
+if Guess <= 10: 
+    print("Guess is too low")
+elif Guess >= 30: 
+    print("Your is too High ")
+elif Guess == 20: 
+    print("Nice you corrected")
+
---
+++ file.py
@@ -0,0 +1,47 @@
+phone_balance = 4
+bank_balance = 100
+if phone_balance < 5: 
+    phone_balance += 10 
+    bank_balance -= 10
+print(phone_balance,bank_balance)
+
+
+phone_balance = 8
+bank_balance = 100
+if phone_balance < 5: 
+    phone_balance += 10 
+    bank_balance -= 10
+print(phone_balance,bank_balance)
+
+
+season = "Summer"  
+if season == "Spring"  : 
+   print("Plant Garden..")
+elif season == "Summer": 
+    print("Water Garden")
+
+elif season == "fall": 
+    print("Harvest the Garden..")
+
+elif season == "Winter":
+    print("Stay indoor..")
+else:
+    print("Unrecongnized Season")
+
+
+Points = 51
+
+if Points <= 50: 
+    result = "Congratulations! You won a wooden rabbit" 
+
+elif Points <= 150:
+    result = "Oh dear, no prize this time !."
+
+elif Points <= 180: 
+    result = "Congratulations! You won a Water-thin mint !"
+else: 
+    result = "Congratulations! You won a Penguin!"
+
+print(result)
+
+
