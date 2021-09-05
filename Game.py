60373c64e3ff3b545746871f81f6410bfea515fe
Sunday AbamJnr <abamsunday5@gmail.com>
2021-09-05 12:49:52 +0100

    second

--- Game.py
+++ Game.py
@@ -11,3 +11,25 @@ elif Guess >= 30:
 elif Guess == 20: 
     print("Nice you corrected")
 
+
+
+State = "Lagos", 10.5
+state = "Abuja", 8.5
+state = "Port Harcourt", 7.5
+amount_of_purchase = "$10"
+if state == "Lagos":
+    total_tax = 0.75
+    total_cost = amount_of_purchase *(1 + total_tax)
+    cost = "Since you're from  {}, your total cost is {}.". format(state,total_cost)
+
+elif state == "Abuja":
+    total_tax = 0.95
+    total_cost = amount_of_purchase * (1 + total_tax)
+    cost = "Since you're from {}, your total cost is {}.".format(state,total_cost)
+
+elif state == "Port Harcourt":
+    total_tax = 0.89
+    total_cost = amount_of_purchase * (1 + total_tax)
+    cost = "Since you're  from {}, your total cost is {}.".format(state,total_tax)
+
+print(cost)
