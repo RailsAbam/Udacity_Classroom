8d012150d36e53ab75ee0a7e966214745eb0379a
Sunday AbamJnr <abamsunday5@gmail.com>
2021-09-07 00:56:46 +0100

    commt

---
+++ bool.py
@@ -0,0 +1,12 @@
+weight = 10
+height = 15
+
+is_raining = False
+is_sunny = True
+
+if 19.5 <= weight / height **2 < 25:
+    print("BMI is considered Normal")
+
+if is_raining and is_sunny:
+    print("Is there a rainbow")
+
