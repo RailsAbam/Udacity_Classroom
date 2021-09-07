a5d80b101015e16335fe450cf7c4f5af586cbcff
Sunday AbamJnr <abamsunday5@gmail.com>
2021-09-07 10:26:27 +0100

    commiting loop

---
+++ loop.py
@@ -0,0 +1,16 @@
+
+#For loop
+
+
+cities = ["lagos", "abuja", "port harcort", "calaber"]
+for index in range(len(cities)):
+    cities[index] = cities[index].title()
+print(cities)
+
+
+states = ["benue", "ogun", "oyo", "enugu"]
+capitalized_states = [] #Creeting a new list
+for city in range (len(states)):
+    capitalized_states.append(city) #adding the new list created
+    states[index] = states[index].title()
+print(states)
