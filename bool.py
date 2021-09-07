395a31455ab56d929d1c51ca04b1fc20047f34b9
Sunday AbamJnr <abamsunday5@gmail.com>
2021-09-07 01:24:08 +0100

    commit

--- bool.py
+++ bool.py
@@ -1,12 +1,19 @@
-weight = 10
-height = 15
 
-is_raining = False
-is_sunny = True
 
-if 19.5 <= weight / height **2 < 25:
-    print("BMI is considered Normal")
+altitude = 10000
+speed = 250
+propulsion = "propeller"
 
-if is_raining and is_sunny:
-    print("Is there a rainbow")
 
+if altitude < 10000 and speed > 100:
+    print("Land") #False
+
+if (propulsion == "Jet" or propulsion == "TurboProp") and speed < 300 and altitude > 20000:
+    print("Take off") #False
+
+
+if not (speed > 400 and propulsion == "propeller" ):
+    print("Fail Landing") #True
+
+if (altitude > 500 and speed > 100) or not propulsion == "propeller":
+    print(False)    #True
