7023dea344c378c182b52bd9db9a460b5eb75a97
Sunday AbamJnr <abamsunday5@gmail.com>
2021-09-11 23:22:10 +0100

    committing changes

---
+++ dict.py
@@ -0,0 +1,24 @@
+
+
+cast = {
+           "Jerry Seinfeld": "Jerry Seinfeld",
+           "Julia Louis-Dreyfus": "Elaine Benes",
+           "Jason Alexander": "George Costanza",
+           "Michael Richards": "Cosmo Kramer"
+       }
+
+for (key,value) in cast.items():
+    print("Actor : {}  Role:{}".format(key,value))
+
+
+
+result = 0
+basket_items = {'apples': 4, 'oranges': 19, 'kites': 3, 'sandwiches': 8}
+fruits = ['apples', 'oranges', 'pears', 'peaches', 'grapes', 'bananas']
+
+for object,count in basket_items.items():
+    if object in fruits:
+        result += count
+print("There are {} friuts in the basket".format(result))
+
+
