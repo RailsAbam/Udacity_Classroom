f6ce8d78bb492a44a0722a51a4faac33b29ec04b
Rails Abam Dev <Rails Aabam@RailsAbamDev.Rails>
2021-09-13 15:06:59 +0100

    commit

--- Edureka/com.py
+++ Edureka/com.py
@@ -1,18 +1,25 @@
-#import math
-
-#volume = 4 /3(math.pi) * r**3
-#def print_volume (r):
- #   print(volume)
-#print(print_volume(7))
 
 
 my_list = [10, 15, "minuted", 50, "seconds", 28, "Rails"] 
 
 
+my_list[2] = 100 #Updating the  SECOND item in  this list
+my_list .append(150)      #Adding an item to the END  list
+my_list.insert(5, 250) #Adding an item in the index 5 part of the list
+my_list.reverse()
+print(my_list)
+
+
+#List 
+
+#animals = {"reptiles": "snakes", "mammals": "whales", "amphibians":"frogs"}
+courses = {1: "Python", 2:"Ruby", 3: "JavaScript", "four": "data science"}
+courses[4] = "machine learning" #Updating the dictionary
+print(courses)
+
+
+#Tuples
 
 
 
 
-
-
-print(my_list[2:5])
---
+++ Edureka/pi.py
@@ -0,0 +1,12 @@
+import math
+
+
+r = 1
+#(math.pi) * r **3
+#You can implement it this way
+def print_volume(r):
+    volume = 4 / 3
+    print(volume * (math.pi) * r **3)
+print(print_volume(20))
+
+
