5bbb20877cd732f4966f4b3b8b68afa4dae8b2a9
Rails Abam <abamsunday5@gmail.com>
2021-09-14 00:34:25 +0100

    commit

--- Edureka/tup.py
+++ Edureka/tup.py
@@ -20,3 +20,13 @@ tech.appendleft("name") #This function adds value to the FRONT of the deque
 tech.pop() #Remove the last element on the list
 tech.popleft() #This remove the FIRST element on the deque list
 print(tech)
+
+#ChainMap
+dict1 = {1:"Django", 2: "Python"}
+dict2 = {3: "Rail", 4 : "Ruby"}
+
+two_dict = ChainMap(dict1, dict2) #This Joint the two Dictionary 
+print(two_dict)
+
+
+
