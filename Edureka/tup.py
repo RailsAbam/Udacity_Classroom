225ae2e0a6df6fc51939047979172b507b785e31
Rails Abam <abamsunday5@gmail.com>
2021-09-14 00:52:25 +0100

    commit

--- Edureka/tup.py
+++ Edureka/tup.py
@@ -32,8 +32,10 @@ print(two_dict)
 #Counter
 num = [1,2,1, 3, 2, 3, 4,2, 4, 1, 5, 4,5, 6, 5, 6,4,7, 8,7,6]
 sum_1 = Counter(num) #This count how many times each value appears
-ele = list(sum_1.elements())
-print(ele)
+element = list(sum_1.elements()) #This return elements in the list in how many they appear
+element_1 = sum_1.most_common()
+print(element)
+print(element_1)
 print(sum_1)
 
 
