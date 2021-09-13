54f685179ab7a068b621d2931d5dc49991b8ee88
Rails Abam Dev <Rails Aabam@RailsAbamDev.Rails>
2021-09-14 00:21:55 +0100

    commiting new changes

--- Edureka/tup.py
+++ Edureka/tup.py
@@ -1,5 +1,8 @@
 
 from collections import namedtuple
+from collections import  deque
+from collections import ChainMap
+from collections import Counter
 
 lang = namedtuple("course", "name, technology")
 stack1 = lang("data science","python" )
@@ -8,3 +11,12 @@ print(stack1)
 lang1 = namedtuple("course", "name, technology")
 stack = lang1._make(["Django", "Python"])
 print(stack)
+
+#Deque
+name = ['R', 'a', 'i', 'l']
+tech = deque(name)
+tech.append("and") #Adding value at the end of the deque
+tech.appendleft("name") #This function adds value to the FRONT of the deque
+tech.pop() #Remove the last element on the list
+tech.popleft() #This remove the FIRST element on the deque list
+print(tech)
