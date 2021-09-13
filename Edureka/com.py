10da6c84f4e48f75020e7768bbb9e9701e45ba79
Rails Abam Dev <Rails Aabam@RailsAbamDev.Rails>
2021-09-13 12:44:51 +0100

    commit

---
+++ Edureka/com.py
@@ -0,0 +1,18 @@
+#import math
+
+#volume = 4 /3(math.pi) * r**3
+#def print_volume (r):
+ #   print(volume)
+#print(print_volume(7))
+
+
+my_list = [10, 15, "minuted", 50, "seconds", 28, "Rails"] 
+
+
+
+
+
+
+
+
+print(my_list[2:5])
